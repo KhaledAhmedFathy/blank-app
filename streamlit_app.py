0a3f8ef1e5fbd23fb1df97a2d9a96c6ba13401c2
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-02 16:01:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 st.title("🎈 Transformers Learning Challenge")
 st.write("Khaled Fathy")
-
-from transformers import pipeline
-
-summarizer = pipeline("summarization", model="t5-small")
+st.summarizer = pipeline("summarization", model="t5-small")
 
