28dff198ad7a94703f672325eb0696cf699bea75
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-02 15:40:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 st.title("🎈 Transformers Learning Challenge")
 st.write("Khaled Fathy")
-
-# Prompt the user for input
-st.user_input = simpledialog.askstring(title="Input", prompt="Please enter your input:")
-
 # Print the user input
 st.print("You entered:", user_input)
 
