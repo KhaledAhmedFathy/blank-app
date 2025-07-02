588942a38191c1efc7a046aca087bfc68281068e
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-02 18:22:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 st.title("🎈 Transformers Learning Challenge")
 st.write("Khaled Fathy")
-
+!npm install localtunnel
+ !pip install pyngrok
+        # Then, after importing, set your auth token:
+        # from pyngrok import ngrok
+        # ngrok.set_auth_token("YOUR_NGROK_AUTH_TOKEN")
+    !streamlit run app.py &>/content/logs.txt &
+        !npx localtunnel --port 8501
+  # from pyngrok import ngrok
+        # public_url = ngrok.connect(port="8501")
+        # print(public_url)
