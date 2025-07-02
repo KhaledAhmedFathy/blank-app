d913f54a52e002cfedaa1609f4aa278cd49ad3e7
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-02 15:33:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("🎈 Transformers Learning Challenge")
 st.write("Khaled Fathy")
 
-user_input = input("Please enter your input: ")
+user_input = st.input("Please enter your input: ")
 st.print("You entered:", user_input)
 
