56e2f670bcfdfb9f3099ea7edc05c1d85bb671da
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-03 15:50:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("🎈AI Summarization Web App")
 st.write("Past your text & click summarize")
-user_input = st.text_input("What do you need to summarize")
+user_input = st.text_input("Text")
 x = st.button("Summarize")
 print(x)
 AI_output = st.text_input("Summary")
