64b6222477fd5fb110b4b7b09577d0589dde1ea5
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-03 18:22:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.write("Past your text & click summarize")
 user_input = st.text_input("Text")
 x = st.button("Summarize")
 print(x)
-AI_output = st.text_input("Summary")
+##AI_output = st.text_input("Summary")
 st.write(user_input)
 
 prompt = "summarize:" + user_input
