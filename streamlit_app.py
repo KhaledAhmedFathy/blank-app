c7427e944147cb6d597e58684c91c82280bfcc9d
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-03 12:26:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title("🎈 Transformers Learning Challenge")
 st.write("Khaled Fathy")
-
+summarizer = pipeline("summarization", model="Falconsai/text_summarization")
+print(summarizer(ARTICLE, max_length=1000, min_length=30, do_sample=False))
