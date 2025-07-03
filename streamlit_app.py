8126237d63dec49d848f93c7aa3bc80ec7bee001
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-03 18:18:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,12 @@ user_input = st.text_input("Text")
 x = st.button("Summarize")
 print(x)
 AI_output = st.text_input("Summary")
+st.write(user_input)
 
 prompt = "summarize:" + user_input
 input = prompt
 print(input)
-##Summary = summarizer(input,max_length=70)
-##print(output)
-##st.write(output)
+Summary = summarizer(input,max_length=70)
+print(output)
+st.write(output)
 
