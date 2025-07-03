9af500c9d6d3db1c2f04110f9e7bd400bc1d7210
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-03 15:28:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("🎈 Transformers Learning Challenge")
 st.write("Khaled")
-x = st.button("What do you need to summerize?")
+x = st.button("What do you need to summarize?")
 print(x)
 y = st.button("Summary")
 print(y)
