74a9c627096584ba74d694b4b7538eb2867646a7
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-03 15:48:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 st.title("🎈AI Summarization Web App")
-st.write("Developed by Khaled Fathy")
-
-y = st.button("Summary")
-print(y)
+st.write("Past your text & click summarize")
 user_input = st.text_input("What do you need to summarize")
 x = st.button("Summarize")
 print(x)
+AI_output = st.text_input("Summary")
 
 
