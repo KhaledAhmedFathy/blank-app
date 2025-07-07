c750b112d544c970f1e5017d2032fe651759f0f6
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-07 13:21:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,19 +6,10 @@ user_input = st.text_input("Text")
 x = st.button("Summarize")
 print(x)
 #AI_output = st.text_input("Summary")
-st.write(user_input,max_length=10)
+st.write(user_input,max_length=5)
 prompt = "summarize:" + user_input
 input = prompt
 print(input)
 ##Summary = summarizer(input,max_length=70)
 ##print(output)
 
-import Flask
-app = Flask(__name__)
-HTML = """https://blank-app-oqw4o31zmd.streamlit.app/"""
-#@app.route("/", methods=["GET", "POST"])
-#def index():
-#output = user_input
-#if request.method == "POST":
-#output = "Button was pressed!"
-#return render_template_string(HTML, output=output)
