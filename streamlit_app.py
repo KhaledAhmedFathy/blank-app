a1c4f99963a6a298d1899efbf5ce2a121baa4def
KhaledAhmedFathy <98961929+KhaledAhmedFathy@users.noreply.github.com>
2025-07-08 15:07:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 st.title("🎈AI Summarization Web App")
 st.write("Past your text & click summarize")
+
 user_input = st.text_input("Text")
 x = st.button("Summarize")
-st.click(x)
 print(x)
-#AI_output = st.text_input("Summary")
 st.write(user_input,max_length=5)
-prompt = "summarize:" + user_input
-input = prompt
-print(input)
+#prompt = "summarize:" + user_input
+#input = prompt
+#print(input)
+
+
 ##Summary = summarizer(input,max_length=70)
 ##print(output)
+#AI_output = st.text_input("Summary")
 
